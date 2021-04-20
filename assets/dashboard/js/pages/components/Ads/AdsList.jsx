e4e0bdcfd065092ff51824822b6a1a9b7e79305f
Shanbora <chanbora@logilink.fr>
2021-04-20 15:41:05 +0200

    add choice to switch list or map

--- assets/dashboard/js/pages/components/Ads/AdsList.jsx
+++ assets/dashboard/js/pages/components/Ads/AdsList.jsx
@@ -8,24 +8,27 @@ import { Alert }      from "@dashboardComponents/Tools/Alert";
 
 import { AdsItem }   from "./AdsItem";
 import { AdsSearch } from "./AdsSearch";
-import {MapGroup} from "./split/MapGroup";
+import { MapGroup }  from "./split/MapGroup";
 
 export class AdsList extends Component {
     constructor(props) {
         super(props);
 
         this.state = {
+            context: "list",
             nature: "Location"
         }
 
         this.handleNature = this.handleNature.bind(this);
+        this.handleChangeContext = this.handleChangeContext.bind(this);
     }
 
     handleNature = (label) => { this.setState({ nature: label }) }
+    handleChangeContext = (context) => { this.setState({ context }) }
 
     render () {
         const { data } = this.props;
-        const { nature } = this.state;
+        const { context, nature } = this.state;
 
         let currentData = [];
         if(data && data.length !== 0){
@@ -36,37 +39,52 @@ export class AdsList extends Component {
             })
         }
 
+        let contexts = [
+            {id: 0, icon: 'tasks', context: 'list'},
+            {id: 1, icon: 'placeholder', context: 'map'}
+        ]
+
+        console.log(context)
+
         return <>
             <div>
                 <div className="toolbar">
+                    <div className="item display-mode">
+                        {contexts.map(c => {
+                            let active = context === c.context ? " active" : "";
+                            return <div className={active} onClick={() => this.handleChangeContext(c.context)} key={c.id}>
+                                <span className={"icon-" + c.icon} />
+                            </div>
+                        })}
+                    </div>
                     <div className="item ads-search">
                         <AdsSearch {...this.state} onNature={this.handleNature}/>
                     </div>
                 </div>
 
-               <MapGroup elems={currentData}/>
-
-                <div className="items-table">
-                    <div className="items items-default items-user items-ad">
-                        <div className="item item-header">
-                            <div className="selector">#</div>
-                            <div className="item-content">
-                                <div className="item-body">
-                                    <div className="avatar" />
-                                    <div className="infos">
-                                        <div className="ad-info">Bien</div>
-                                        <div className="ad-price">Prix</div>
-                                        <div className="ad-ad">Type annonce</div>
-                                        <div className="ad-bien">Type bien</div>
+                {context === "map" ? <MapGroup elems={currentData}/> : <>
+                    <div className="items-table">
+                        <div className="items items-default items-user items-ad">
+                            <div className="item item-header">
+                                <div className="selector">#</div>
+                                <div className="item-content">
+                                    <div className="item-body">
+                                        <div className="avatar" />
+                                        <div className="infos">
+                                            <div className="ad-info">Bien</div>
+                                            <div className="ad-price">Prix</div>
+                                            <div className="ad-ad">Type annonce</div>
+                                            <div className="ad-bien">Type bien</div>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
+                            {currentData && currentData.length !== 0 ? currentData.map(elem => {
+                                return <AdsItem {...this.props} elem={elem} key={elem.id}/>
+                            }) : <Alert>Aucun résultat</Alert>}
                         </div>
-                        {currentData && currentData.length !== 0 ? currentData.map(elem => {
-                            return <AdsItem {...this.props} elem={elem} key={elem.id}/>
-                        }) : <Alert>Aucun résultat</Alert>}
                     </div>
-                </div>
+                </>}
 
                 <div className="page-actions">
                     <div className="common-actions">
