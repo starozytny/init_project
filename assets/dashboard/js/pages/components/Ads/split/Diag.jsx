7198b3ac7a5307130c952cdfb4dfccd9746b0f00
Shanbora <chanbora@logilink.fr>
2021-04-20 11:54:43 +0200

    pattern diag

--- assets/dashboard/js/pages/components/Ads/split/Diag.jsx
+++ assets/dashboard/js/pages/components/Ads/split/Diag.jsx
@@ -4,6 +4,15 @@ import Sanitize  from "@dashboardComponents/functions/sanitaze";
 export function Diag({ elem }){
 
     let letters = ["A", "B", "C", "D", "E", "F", "G"];
+    let lettersDetails = [
+        { le :"A", valDpe: "≤ 50", valGes: "≤ 5" },
+        { le :"B", valDpe: "51 à 90", valGes: "6 à 10" },
+        { le :"C", valDpe: "91 à 150", valGes: "11 à 20" },
+        { le :"D", valDpe: "151 à 230", valGes: "21 à 35" },
+        { le :"E", valDpe: "231 à 330", valGes: "36 à 55" },
+        { le :"F", valDpe: "331 à 450", valGes: "56 à 80" },
+        { le :"G", valDpe: "> 450", valGes: "> 80" }
+    ]
 
     return (<div className="details-tab-infos">
 
@@ -23,6 +32,22 @@ export function Diag({ elem }){
                     </div>
                 </div>
             </div>
+            <div className="diagnostic-details">
+                <div>
+                    <div className="diag-title">Diagnostic de performance énergétique En kWhEP/m2.an</div>
+                    <div className="diag-dpe">
+                        {lettersDetails.map(le => {
+                            return <div key={le.le}>
+                                <div className={"dpe dpe-" + le.le.toLowerCase()}>
+                                    <div>{le.valDpe}</div>
+                                    <div>{le.le}</div>
+                                </div>
+                                <div className="number">5</div>
+                            </div>
+                        })}
+                    </div>
+                </div>
+            </div>
         </div>
 
         <div className="details-tab-infos-main">
@@ -34,6 +59,22 @@ export function Diag({ elem }){
                             return <div key={le}>
                                 <div className={"ges ges-" + le.toLowerCase()}>
                                     <div>{le}</div>
+                                </div>
+                                <div className="number">5</div>
+                            </div>
+                        })}
+                    </div>
+                </div>
+            </div>
+            <div className="diagnostic-details">
+                <div>
+                    <div className="diag-title">Indice d'émission de gaz à effet de serre En kWhEP/m2.an</div>
+                    <div className="diag-ges">
+                        {lettersDetails.map(le => {
+                            return <div key={le.le}>
+                                <div className={"ges ges-" + le.le.toLowerCase()}>
+                                    <div>{le.valGes}</div>
+                                    <div>{le.le}</div>
                                 </div>
                                 <div className="number">5</div>
                             </div>
