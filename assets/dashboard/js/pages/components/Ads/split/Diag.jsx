be12d72a2cab12dc57285766bc9f309c3b6a6035
Shanbora <chanbora@logilink.fr>
2021-04-20 11:44:59 +0200

    add first pattern diag

--- assets/dashboard/js/pages/components/Ads/split/Diag.jsx
+++ assets/dashboard/js/pages/components/Ads/split/Diag.jsx
@@ -2,12 +2,44 @@ import React     from "react";
 import Sanitize  from "@dashboardComponents/functions/sanitaze";
 
 export function Diag({ elem }){
+
+    let letters = ["A", "B", "C", "D", "E", "F", "G"];
+
     return (<div className="details-tab-infos">
 
         <div className="details-tab-infos-main">
-            <div>
-                <div className="label">Diag</div>
-                <div>Diag</div>
+            <div className="diagnostic">
+                <div>
+                    <div className="diag-title">Diagnostic de performance énergétique</div>
+                    <div className="diag-dpe">
+                        {letters.map(le => {
+                            return <div key={le}>
+                                <div className={"dpe dpe-" + le.toLowerCase()}>
+                                    <div>{le}</div>
+                                </div>
+                                <div className="number">5</div>
+                            </div>
+                        })}
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div className="details-tab-infos-main">
+            <div className="diagnostic">
+                <div>
+                    <div className="diag-title">Indice d'émission de gaz à effet de serre</div>
+                    <div className="diag-ges">
+                        {letters.map(le => {
+                            return <div key={le}>
+                                <div className={"ges ges-" + le.toLowerCase()}>
+                                    <div>{le}</div>
+                                </div>
+                                <div className="number">5</div>
+                            </div>
+                        })}
+                    </div>
+                </div>
             </div>
         </div>
     </div>)
