a9c949f65e74e129ce4b381e15ad08c4072e7c3e
Shanbora <chanbora@logilink.fr>
2021-04-20 09:20:51 +0200

    change var to let

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
-var Encore = require('@symfony/webpack-encore');
-var path   = require('path');
+let Encore = require('@symfony/webpack-encore');
+let path   = require('path');
 
 // Manually configure the runtime environment if not already configured yet by the "encore" command.
 // It's useful when you use tools that rely on webpack.config.js file.
