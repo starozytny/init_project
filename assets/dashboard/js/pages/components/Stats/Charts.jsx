87431c8508efc65dc0456999aab1c8669892144d
Shanbora <chanbora@logilink.fr>
2021-07-23 12:06:35 +0200

    style charts responsive

--- assets/dashboard/js/pages/components/Stats/Charts.jsx
+++ assets/dashboard/js/pages/components/Stats/Charts.jsx
@@ -119,6 +119,23 @@ export class ChartBiens extends Component {
                         height: 200,
                         type: 'treemap',
                         toolbar: { show: false },
+                    },
+                    colors: [
+                        '#3B93A5',
+                        '#F7B844',
+                        '#ADD8C7',
+                        '#EC3C65',
+                        '#CDD7B6',
+                        '#C1F666',
+                        '#D43F97',
+                        '#1E5D8C',
+                        '#421243',
+                    ],
+                    plotOptions: {
+                        treemap: {
+                            distributed: true,
+                            enableShades: false
+                        }
                     }
                 },
             })
