d48ca3079d00500ead6fee6f8c96cbbabadd5600
Shanbora <chanbora@logilink.fr>
2021-12-10 15:34:34 +0100

    display duration by days

--- assets/dashboard/js/pages/components/Formations/Sessions/helpFunction.js
+++ assets/dashboard/js/pages/components/Formations/Sessions/helpFunction.js
@@ -67,61 +67,6 @@ function getDurationTotal(duration, duration2)
     return setToString(nDuration[0] + nDuration2[0], nDuration[1] + nDuration2[1]);
 }
 
-/**
- * start doit être inférieur à end
- * @param startArray - array [d,m,y]
- * @param endArray - array [d,m,y]
- */
-function getNbDayBetweenDate(startArray, endArray)
-{
-    let startYear  = startArray[2];
-    let startMonth = startArray[1];
-    let startDay   = startArray[0];
-    let endYear    = endArray[2];
-    let endMonth   = endArray[1];
-    let endDay     = endArray[0];
-
-    let nbDaysByMonth = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
-
-    let nbYears = endYear - startYear
-
-    let days = 0;
-    if(nbYears >= 0){
-        if(nbYears === 0){ // même année
-            if(startMonth === endMonth){ // même mois
-                days = endDay - startDay;
-                days = days === 0 ? 1 : days;
-            }else if(startMonth < endMonth){ // classique date
-                days = days + (nbDaysByMonth[startMonth] - startDay + 1); //calcul en fonction du nb de jours dans le current mois
-                days = days + endDay;
-                for(let i = startMonth + 1 ; i < endMonth ; i++){ //ajout des jours entre les mois exclusion mois start et end
-                    days = days + nbDaysByMonth[i];
-                }
-            }
-        }else{
-            //remplir les jours du current start mois et end mois
-            days = days + (nbDaysByMonth[startMonth] - startDay + 1);
-            days = days + endDay;
-
-            //remplir les mois restants du start jusqu'a la nouvelle année
-            for(let i = startMonth + 1 ; i <= 12 ; i++){
-                days = days + nbDaysByMonth[i];
-            }
-            //remplir les mois avant end mois
-            for(let i = 1 ; i < endMonth ; i++){
-                days = days + nbDaysByMonth[i];
-            }
-
-            //remplir les années restantes exclusion année start et end
-            for(let i = startYear + 1 ; i < endYear ; i++){
-                days = days + 365;
-            }
-        }
-    }
-
-    return days;
-}
-
 function getDurationByDay(duration, duration2, start, end)
 {
     if(start !== null && start !== "" && end !== null && end !== ""){
@@ -134,7 +79,16 @@ function getDurationByDay(duration, duration2, start, end)
         let startArray = Helper.extractDateToArray(start);
         let endArray = Helper.extractDateToArray(end);
 
-        return getNbDayBetweenDate(startArray, endArray)
+        let nbDays = Helper.getNbDayBetweenDateArray(startArray, endArray)
+
+        let total = (nbHours * 60 + nbMinutes) * nbDays;
+
+        let hours = Math.floor(total / 60);
+        let minutes = total % 60;
+
+        console.log(nbDays)
+
+        return setToString(hours, minutes);
     }
 
     return "";
