8f87c8b4ba90b711ed79753d9e8bfb36b4f21936
Shanbora <chanbora@logilink.fr>
2021-12-13 11:45:38 +0100

    add delete worker

--- src/Controller/Api/Formation/TeamController.php
+++ src/Controller/Api/Formation/TeamController.php
@@ -109,4 +109,30 @@ class TeamController extends AbstractController
     {
         return $this->submitForm("update", $obj, $request, $apiResponse, $validator, $dataEntity);
     }
+
+    /**
+     * Delete a worker
+     *
+     * @Route("/{id}", name="delete", options={"expose"=true}, methods={"DELETE"})
+     *
+     * @OA\Response(
+     *     response=200,
+     *     description="Return message successful",
+     * )
+     *
+     * @OA\Tag(name="Team")
+     *
+     * @param FoWorker $obj
+     * @param ApiResponse $apiResponse
+     * @return JsonResponse
+     */
+    public function delete(FoWorker $obj, ApiResponse $apiResponse): JsonResponse
+    {
+        $em = $this->doctrine->getManager();
+
+        $em->remove($obj);
+        $em->flush();
+
+        return $apiResponse->apiJsonResponseSuccessful("Supression réussie !");
+    }
 }
