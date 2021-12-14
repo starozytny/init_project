f95873be22f98b0e902a7287bba1c82bc9076df3
Shanbora <chanbora@logilink.fr>
2021-12-14 12:10:34 +0100

    preapare path to list participants of sessions to get attestations

--- assets/dashboard/js/pages/components/Formations/Sessions/SessionsItem.jsx
+++ assets/dashboard/js/pages/components/Formations/Sessions/SessionsItem.jsx
@@ -2,12 +2,21 @@ import React, { Component } from 'react';
 
 import Routing          from '@publicFolder/bundles/fosjsrouting/js/router.min.js';
 
-import { ButtonIcon }   from "@dashboardComponents/Tools/Button";
+import {ButtonIcon, ButtonIconDropdown} from "@dashboardComponents/Tools/Button";
 import { Selector }     from "@dashboardComponents/Layout/Selector";
 
 export class SessionsItem extends Component {
     render () {
         const { elem, onChangeContext, onDelete, onSelectors, onSwitchPublished } = this.props
+
+        let actions = [
+            {data: <ButtonIcon element="a" icon="download" target="_blank"
+                               onClick={Routing.generate('api_sessions_emargements', {'slug': elem.slug})}
+                               text="Emargements" />},
+            {data: <ButtonIcon element="a" icon="user"
+                               onClick={Routing.generate('api_sessions_emargements', {'slug': elem.slug})}
+                               text="Participants" />},
+        ]
 
         return <div className="item">
             <Selector id={elem.id} onSelectors={onSelectors} />
@@ -24,16 +33,12 @@ export class SessionsItem extends Component {
                             <div className="sub" />
                         </div>
                         <div className="col-3 actions">
-                            <ButtonIcon element="a" icon="download" target="_blank"
-                                        onClick={Routing.generate('api_sessions_emargements', {'slug': elem.slug})}
-                            >
-                                Emargements
-                            </ButtonIcon>
                             <ButtonIcon icon={elem.isPublished ? "vision" : "vision-not"} onClick={() => onSwitchPublished(elem)}>
                                 {elem.isPublished ? "En ligne" : "Hors ligne"}
                             </ButtonIcon>
                             <ButtonIcon icon="pencil" onClick={() => onChangeContext("update", elem)}>Modifier</ButtonIcon>
                             <ButtonIcon icon="trash" onClick={() => onDelete(elem)}>Supprimer</ButtonIcon>
+                            <ButtonIconDropdown icon="menu" items={actions}>Autres</ButtonIconDropdown>
                         </div>
                     </div>
                 </div>
