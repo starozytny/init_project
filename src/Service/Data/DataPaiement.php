c388f54481a3f43e2fb627487de30799de1fbd80
Shanbora <chanbora@logilink.fr>
2021-12-15 15:52:27 +0100

    create function se data order

--- src/Service/Data/DataPaiement.php
+++ src/Service/Data/DataPaiement.php
@@ -2,6 +2,9 @@
 
 namespace App\Service\Data;
 
+use App\Entity\Paiement\PaOrder;
+use App\Entity\User;
+
 class DataPaiement
 {
     private $privateDirectory;
@@ -11,12 +14,48 @@ class DataPaiement
         $this->privateDirectory = $privateDirectory;
     }
 
+    /**
+     * @param PaOrder $obj
+     * @param $data
+     * @param User $user
+     * @param $rum
+     * @param $code
+     * @param $ip
+     * @return PaOrder
+     */
+    public function setDataOrder(PaOrder $obj, $data, User $user, $rum, $code, $ip): PaOrder
+    {
+        return ($obj)
+            ->setRum($rum)
+            ->setPrice((float) $data->price)
+            ->setName(trim($data->name))
+            ->setTitulaire(trim($data->titulaire))
+            ->setIban(trim($data->iban))
+            ->setBic(trim($data->bic))
+            ->setEmail(trim($data->email))
+            ->setCode($code)
+            ->setParticipants((int) $data->participants)
+            ->setIp($ip)
+            ->setAddress(trim($data->address))
+            ->setZipcode((int) $data->zipcode)
+            ->setCity(trim($data->city))
+            ->setUser($user)
+        ;
+    }
+
+    /**
+     * @param $filename
+     * @return string
+     */
     public function getFile($filename): string
     {
         $path = $this->getPrivateDirectory() . "paiements";
         return $path . "/" .$filename;
     }
 
+    /**
+     * @return string
+     */
     public function getPaiementDirectory(): string
     {
         return $this->getPrivateDirectory() . "paiements";
