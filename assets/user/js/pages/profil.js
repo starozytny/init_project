bc2b8c47faab201418eb668500762008275ce190
Shanbora <chanbora@logilink.fr>
2022-01-14 10:17:07 +0100

    reorganise components user

--- assets/user/js/pages/profil.js
+++ assets/user/js/pages/profil.js
@@ -5,10 +5,10 @@ import Routing from '@publicFolder/bundles/fosjsrouting/js/router.min';
 
 import React from "react";
 import { render } from "react-dom";
-import { UserFormulaire } from "@userComponents/Profil/UserForm";
-import { BankFormulaire } from "@userComponents/Profil/Bank/BankForm";
-import { Banks }          from "@userComponents/Profil/Bank/Banks";
-import { Orders }         from "@userComponents/Profil/Order/Orders";
+import { UserFormulaire } from "@userPages/components/Profil/UserForm";
+import { BankFormulaire } from "@userPages/components/Profil/Bank/BankForm";
+import { Banks }          from "@userPages/components/Profil/Bank/Banks";
+import { Orders }         from "@userPages/components/Profil/Order/Orders";
 
 Routing.setRoutingData(routes);
 
