136a366e3f42eba10c9ff05fd7bfcc7da915caae
Shanbora <chanbora@logilink.fr>
2022-03-09 11:17:32 +0100

    numero to brouillon

--- src/Controller/Api/Bill/InvoiceController.php
+++ src/Controller/Api/Bill/InvoiceController.php
@@ -47,8 +47,7 @@ class InvoiceController extends AbstractController
         $obj = $dataEntity->setDataInvoice($obj, $data);
 
         if($type == "create"){
-            $numero = $dataEntity->createNewNumeroBill(0, "BR");
-            $obj->setNumero($numero);
+            $obj->setNumero("Brouillon");
         }else{
             $obj->setUpdatedAt(new \DateTime());
         }
