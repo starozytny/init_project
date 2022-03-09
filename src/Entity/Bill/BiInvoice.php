2dfb650fa253339d5e4ef2d944cbd22ce4395915
Shanbora <chanbora@logilink.fr>
2022-03-09 09:12:51 +0100

    init invoice

---
+++ src/Entity/Bill/BiInvoice.php
@@ -0,0 +1,313 @@
+<?php
+
+namespace App\Entity\Bill;
+
+use App\Repository\Bill\BiInvoiceRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=BiInvoiceRepository::class)
+ */
+class BiInvoice
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $numero;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $dateAt;
+
+    /**
+     * @ORM\Column(type="datetime", nullable=true)
+     */
+    private $dueAt;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $fromName;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $fromAddress;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $fromComplement;
+
+    /**
+     * @ORM\Column(type="string", length=40)
+     */
+    private $fromZipcode;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $fromCity;
+
+    /**
+     * @ORM\Column(type="string", length=60, nullable=true)
+     */
+    private $fromPhone1;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $fromEmail;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $toName;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $toAddress;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $toComplement;
+
+    /**
+     * @ORM\Column(type="string", length=40)
+     */
+    private $toZipcode;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $toCity;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $toEmail;
+
+    /**
+     * @ORM\Column(type="string", length=60, nullable=true)
+     */
+    private $toPhone1;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNumero(): ?int
+    {
+        return $this->numero;
+    }
+
+    public function setNumero(int $numero): self
+    {
+        $this->numero = $numero;
+
+        return $this;
+    }
+
+    public function getDateAt(): ?\DateTimeInterface
+    {
+        return $this->dateAt;
+    }
+
+    public function setDateAt(\DateTimeInterface $dateAt): self
+    {
+        $this->dateAt = $dateAt;
+
+        return $this;
+    }
+
+    public function getDueAt(): ?\DateTimeInterface
+    {
+        return $this->dueAt;
+    }
+
+    public function setDueAt(?\DateTimeInterface $dueAt): self
+    {
+        $this->dueAt = $dueAt;
+
+        return $this;
+    }
+
+    public function getFromName(): ?string
+    {
+        return $this->fromName;
+    }
+
+    public function setFromName(string $fromName): self
+    {
+        $this->fromName = $fromName;
+
+        return $this;
+    }
+
+    public function getFromAddress(): ?string
+    {
+        return $this->fromAddress;
+    }
+
+    public function setFromAddress(string $fromAddress): self
+    {
+        $this->fromAddress = $fromAddress;
+
+        return $this;
+    }
+
+    public function getFromComplement(): ?string
+    {
+        return $this->fromComplement;
+    }
+
+    public function setFromComplement(?string $fromComplement): self
+    {
+        $this->fromComplement = $fromComplement;
+
+        return $this;
+    }
+
+    public function getFromZipcode(): ?string
+    {
+        return $this->fromZipcode;
+    }
+
+    public function setFromZipcode(string $fromZipcode): self
+    {
+        $this->fromZipcode = $fromZipcode;
+
+        return $this;
+    }
+
+    public function getFromCity(): ?string
+    {
+        return $this->fromCity;
+    }
+
+    public function setFromCity(string $fromCity): self
+    {
+        $this->fromCity = $fromCity;
+
+        return $this;
+    }
+
+    public function getFromPhone1(): ?string
+    {
+        return $this->fromPhone1;
+    }
+
+    public function setFromPhone1(?string $fromPhone1): self
+    {
+        $this->fromPhone1 = $fromPhone1;
+
+        return $this;
+    }
+
+    public function getFromEmail(): ?string
+    {
+        return $this->fromEmail;
+    }
+
+    public function setFromEmail(?string $fromEmail): self
+    {
+        $this->fromEmail = $fromEmail;
+
+        return $this;
+    }
+
+    public function getToName(): ?string
+    {
+        return $this->toName;
+    }
+
+    public function setToName(string $toName): self
+    {
+        $this->toName = $toName;
+
+        return $this;
+    }
+
+    public function getToAddress(): ?string
+    {
+        return $this->toAddress;
+    }
+
+    public function setToAddress(string $toAddress): self
+    {
+        $this->toAddress = $toAddress;
+
+        return $this;
+    }
+
+    public function getToComplement(): ?string
+    {
+        return $this->toComplement;
+    }
+
+    public function setToComplement(?string $toComplement): self
+    {
+        $this->toComplement = $toComplement;
+
+        return $this;
+    }
+
+    public function getToZipcode(): ?string
+    {
+        return $this->toZipcode;
+    }
+
+    public function setToZipcode(string $toZipcode): self
+    {
+        $this->toZipcode = $toZipcode;
+
+        return $this;
+    }
+
+    public function getToCity(): ?string
+    {
+        return $this->toCity;
+    }
+
+    public function setToCity(string $toCity): self
+    {
+        $this->toCity = $toCity;
+
+        return $this;
+    }
+
+    public function getToEmail(): ?string
+    {
+        return $this->toEmail;
+    }
+
+    public function setToEmail(?string $toEmail): self
+    {
+        $this->toEmail = $toEmail;
+
+        return $this;
+    }
+
+    public function getToPhone1(): ?string
+    {
+        return $this->toPhone1;
+    }
+
+    public function setToPhone1(?string $toPhone1): self
+    {
+        $this->toPhone1 = $toPhone1;
+
+        return $this;
+    }
+}
