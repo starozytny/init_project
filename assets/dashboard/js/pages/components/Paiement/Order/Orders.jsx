e625f4d9f9a25139234f09beb1fa0913638b631d
Shanbora <chanbora@logilink.fr>
2022-03-10 12:12:15 +0100

    compare createdAt inverse lot and order

--- assets/dashboard/js/pages/components/Paiement/Order/Orders.jsx
+++ assets/dashboard/js/pages/components/Paiement/Order/Orders.jsx
@@ -17,7 +17,7 @@ const URL_DELETE_ELEMENT    = 'api_orders_delete';
 const URL_DELETE_GROUP      = 'api_orders_delete_group';
 const MSG_DELETE_ELEMENT    = 'Supprimer cet ordre ?';
 const MSG_DELETE_GROUP      = 'Aucun ordre sélectionné.';
-const SORTER = Sort.compareCreatedAt;
+const SORTER = Sort.compareCreatedAtInverse;
 
 function filterFunction(dataImmuable, filters){
     let newData = [];
