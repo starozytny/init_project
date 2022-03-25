180131f037b397336fb8f5c9bea006e3683880ea
Shanbora <chanbora@logilink.fr>
2022-03-25 15:29:39 +0100

    add tiret to numero

--- src/Service/Bill/BillService.php
+++ src/Service/Bill/BillService.php
@@ -11,7 +11,7 @@ class BillService
         $tab = array_map('intval', str_split($i));
         $nbZero = 6 - count($tab);
 
-        $counter = $year . str_repeat("0", $nbZero);
+        $counter = $year . "-" . str_repeat("0", $nbZero);
         $counter .= $i;
 
         return $prefix . $counter;
